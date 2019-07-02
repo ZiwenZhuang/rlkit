c1c0e837a6ed4917aca064481b46081870dcc9a9
ZiwenZhuang <leo416850130@outlook.com>
2019-07-02 17:12:33 -0400

    using another logger to record testing rewards

--- scripts/run_goal_conditioned_policy.py
+++ scripts/run_goal_conditioned_policy.py
@@ -1,7 +1,8 @@
 import argparse
 import pickle
-
+from os import path as osp
 from rlkit.core import logger
+from rlkit.core.repeat_logger import RepeatLogger, RepeatPlotter
 from rlkit.samplers.rollout_functions import multitask_rollout
 from rlkit.torch import pytorch_util as ptu
 from rlkit.envs.vae_wrapper import VAEWrappedEnv
@@ -20,8 +21,15 @@ def simulate_policy(args):
     if args.enable_render or hasattr(env, 'enable_render'):
         # some environments need to be reconfigured for visualization
         env.enable_render()
+    if args.log_dir != None:
+        # time to setup logger to dump recordings to file
+        # It should be safer to use absolute directory
+        logger.set_snapshot_dir(osp.abspath(args.log_dir))
+        logger.add_tabular_output('rollouts.csv', relative_to_snapshot_dir=True)
+        success_logger = RepeatLogger(osp.join(osp.abspath(args.log_dir), 'image_success.csv'))
+        success_logger = RepeatLogger(osp.join(osp.abspath(args.log_dir), 'vae_dist.csv'))
     paths = []
-    while True:
+    for _ in range(64): # incase the testing takes too much physical memory
         paths.append(multitask_rollout(
             env,
             policy,
@@ -35,6 +43,10 @@ def simulate_policy(args):
         if hasattr(env, "get_diagnostics"):
             for k, v in env.get_diagnostics(paths).items():
                 logger.record_tabular(k, v)
+        if args.log_dir != None:
+            # this data has to be chosen by specific path field.
+            success_logger.record([paths[-1]['env_infos'][i]['image_success'] for i in range(args.H)])
+            success_logger.record([paths[-1]['env_infos'][i]['vae_dist'] for i in range(args.H)])
         logger.dump_tabular()
 
 
@@ -52,6 +64,8 @@ if __name__ == "__main__":
     parser.add_argument('--gpu', action='store_true')
     parser.add_argument('--enable_render', action='store_true')
     parser.add_argument('--hide', action='store_true')
+    parser.add_argument('--log_dir', type=str, default= None,
+                        help='Specify the log directory, no logging if not')
     args = parser.parse_args()
 
     simulate_policy(args)
