4b039f9a64073713b2b46761fc830660846e0802
ZiwenZhuang <leo416850130@outlook.com>
2019-07-28 20:45:06 -0400

    update logger

--- scripts/run_goal_conditioned_policy.py
+++ scripts/run_goal_conditioned_policy.py
@@ -7,7 +7,6 @@ from rlkit.samplers.rollout_functions import multitask_rollout
 from rlkit.torch import pytorch_util as ptu
 from rlkit.envs.vae_wrapper import VAEWrappedEnv
 import numpy as np
-import matplotlib.pyplot as plt
 import mujoco_py
 import pickle as pkl
 
@@ -20,18 +19,20 @@ def simulate_policy(args):
         data = pickle.load(open(args.file, "rb"))
         policy = data['evaluation/policy']
         env = data['evaluation/env']
-        print("Policy and environment loaded")
+        logger.log("Policy and environment loaded")
     if args.redump:
         # re-dump the data
         with open(args.file, "wb") as f:
             pickle.dump(data, f)
+        logger.log("Finish redump")
     if args.gpu:
         ptu.set_gpu_mode(True)
         policy.to(ptu.device)
     if isinstance(env, VAEWrappedEnv) and hasattr(env, 'mode'):
         env.mode(args.mode)
-    if args.enable_render or hasattr(env, 'enable_render'):
+    if (args.enable_render or hasattr(env, 'enable_render')) and not args.hide:
         # some environments need to be reconfigured for visualization
+        logger.log("Enable Rendering")
         env.enable_render()
     if args.log_dir != None:
         # time to setup logger to dump recordings to file
@@ -41,10 +42,13 @@ def simulate_policy(args):
         success_logger = RepeatLogger(osp.join(osp.abspath(args.log_dir), 'image_success.csv'))
         vae_logger = RepeatLogger(osp.join(osp.abspath(args.log_dir), 'vae_dist.csv'))
         ag_logger = RepeatLogger(osp.join(osp.abspath(args.log_dir), 'effector2goal_distance.csv'))
+        logger.log("Setup loggers")
     if hasattr(env, '_goal_sampling_mode') and env._goal_sampling_mode == 'custom_goal_sampler' and env.custom_goal_sampler == None:
         # This a deep hack, to make the sample directly from env wrapped by image_env
         env.custom_goal_sampler = env._customed_goal_sampling_func
+        logger.log("Change env.custom_goal_sampler to its _customed_goal_sampling_func")
     paths = []
+    logger.log("Start Rollout")
     for ite in range(64): # incase the testing takes too much physical memory
         paths.append(multitask_rollout(
             env,
@@ -54,25 +58,29 @@ def simulate_policy(args):
             observation_key=data['evaluation/observation_key'],
             desired_goal_key=data['evaluation/desired_goal_key'],
         ))
+        logger.log("iter %d: Finish rollout" % ite)
         if hasattr(env, "log_diagnostics"):
             env.log_diagnostics(paths)
+            logger.log("iter %d: Log diagnostics" % ite)
         if hasattr(env, "get_diagnostics"):
             for k, v in env.get_diagnostics(paths).items():
                 logger.record_tabular(k, v)
+            logger.log("iter %d: Get diagnostics" % ite)
         if args.log_dir != None:
             # this data has to be chosen by specific path field.
             success_logger.record([paths[-1]['env_infos'][i]['image_success'] for i in range(len(paths[-1]['env_infos']))])
             vae_logger.record([paths[-1]['env_infos'][i]['vae_dist'] for i in range(len(paths[-1]['env_infos']))])
-            if "ag_state" in paths[-1]['env_infos'][0].keys():
+            if "effector2goal_distance" in paths[-1]['env_infos'][0].keys():
                 goal_dist = [np.linalg.norm(paths[-1]['env_infos'][i]['effector2goal_distance']) for i in range(len(paths[-1]['env_infos']))]
                 ag_logger.record(goal_dist)
-                with open(args.log_dir + "ag_state.csv", "wb+") as f:
+                with open(args.log_dir + "2goal_dist.pkl", "wb+") as f:
                     pkl.dump(goal_dist, f)
+            logger.log("iter %d: Log into files" % ite)
 
         logger.dump_tabular()
         logger.log("Rollout done: # %d" % ite)
 
-    print("Testing learning result done...")
+    logger.log("Testing learning result done...")
 
 if __name__ == "__main__":
 
